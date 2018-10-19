523c8ed359d6ad9f782c32afe4b4556e155f4c77
gandryeanb <gstandrianb@gmail.com>
2018-10-19 09:59:56 +0700

    merging with frans

--- client/src/router.js
+++ client/src/router.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Lobby from './views/Lobby.vue'
+import Room from './views/Room.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'home',
+    component: Home
+  }, {
+    path: '/lobby',
+    name: 'lobby',
+    component: Lobby
+  }, {
+    path: '/room',
+    name: 'room',
+    component: Room
+  }]
 })
